f2bcd291f6c881121ef5266c6bde97b0c2edffd7
vansh-jain2002 <122167639+vansh-jain2002@users.noreply.github.com>
2024-08-18 16:09:11 +0530

    containirinzing application

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,9 +1,31 @@
-const mongoose = require('mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/scholarshipportal" ,{
-   // useUnifiedTopology:true,
-   // useNewUrlParser:true
-}).then(() => {
-    console.log("connection succeful")
-}).catch((e) => {
-    console.log(e)
-})
+// const mongoose = require('mongoose')
+// mongoose.connect("mongodb://127.0.0.1:27017/scholarshipportal" ,{
+//    // useUnifiedTopology:true,
+//    // useNewUrlParser:true
+// }).then(() => {
+//     console.log("connection succeful")
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const dbURI = 'mongodb+srv://vanshjain:vansh%402002@clusrure-0.jsnrs7g.mongodb.net/?retryWrites=true&w=majority';
+
+if (!dbURI) {
+  console.error("Error: MONGODB_URI is not defined in the .env file.");
+  process.exit(1);
+}
+
+const connectWithRetry = () => {
+  console.log('MongoDB connection with retry');
+  mongoose.connect(dbURI).then(() => {
+    console.log('MongoDB is connected');
+  }).catch(err => {
+    console.error('MongoDB connection unsuccessful, retry after 5 seconds. ', err);
+    setTimeout(connectWithRetry, 5000);
+  });
+};
+
+connectWithRetry();
